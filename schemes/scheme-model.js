5e9be7bf76d7701ccb229d73bc6fbbcb4eb0e121
Pere <pere.solaclaver@gmail.com>
2019-11-06 13:26:59 +0000

    findbyid method querying against database

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,14 +1,18 @@
-const db = require('../data/db-config');
+const db = require("../data/db-config");
 
 module.exports = {
-    find
-    // findById,
-    // findSteps,
-    // add,
-    // update,
-    // remove
-}
+  find,
+  findById
+  // findSteps,
+  // add,
+  // update,
+  // remove
+};
 
 function find() {
-    return db('schemes');
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id });
 }
